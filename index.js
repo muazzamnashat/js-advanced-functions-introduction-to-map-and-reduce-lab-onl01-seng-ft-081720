2529fa5cde08fd3781b70639a43df7eaa87f1b11
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-24 19:40:08 -0500

    commit tree 8547940514808fdb60986a5c7d2b64b28eb5d19e

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+const mapToNegativize = (sourceArray)=> {
+    let result= []
+    for (const el of sourceArray){
+        result.push(-1*el) 
+    }
+    return result
+}
+
+const mapToNoChange = (sourceArray)=> {
+    let result= []
+    for (const el of sourceArray){
+        result.push(el) 
+    }
+    return result
+}
+
+const mapToDouble = (sourceArray)=> {
+    let result= []
+    for (const el of sourceArray){
+        result.push(2*el) 
+    }
+    return result
+}
+
+const mapToSquare = (sourceArray)=> {
+    let result= []
+    for (const el of sourceArray){
+        result.push(el**2) 
+    }
+    return result
+}
+
+const reduceToTotal= (sourceArray, startingPoint) =>{
+    let result = startingPoint? startingPoint : 0; 
+    for (const el of sourceArray){
+        result += el 
+    }
+    return result
+}
+
+const reduceToAllTrue = (sourceArray) =>{
+    for (const el of sourceArray){
+       if (el === false) return false
+    }
+    return true
+}
+
+const reduceToAnyTrue = (sourceArray) =>{
+    for (const el of sourceArray){
+       if (el === true) return true
+    }
+    return false
+}
